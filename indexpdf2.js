900a00eaa1f8dbf4bb3417fb7a9a7fe25b763312
Shaikh Asim <sheikhasim10000@gmail.com>
2020-09-26 00:07:13 +0500

    basic structure for wesco

--- indexpdf2.js
+++ indexpdf2.js
@@ -1,12 +1,43 @@
 const PDFExtract = require('pdf.js-extract').PDFExtract;
 const pdfExtract = new PDFExtract();
 const options = {}; /* see below */
-pdfExtract.extract('pdf2[2].pdf', options, (err, data) => {
+pdfExtract.extract('pdf2[3].pdf', options, (err, data) => {
     if (err) return console.log(err);
-    console.log(JSON.stringify(data));
+    //console.log(JSON.stringify(data));
     parsePdfData(data);
 });
 
-function parsePdfData(){
+function parsePdfData(pdfContent) {
+    let pageContent = pdfContent.pages[0].content;
+    let finalpdfObject={};
+    pageContent.forEach(singleObject => {
+        if(checkPositionXandY(67.5,202.5,singleObject)){
+            finalpdfObject.soldTo=getStringFromObject(singleObject);
+        }
+
+    });
+
+    console.log(JSON.stringify(finalpdfObject));
+}
+
+function getStringFromObject(pdfContentObject) {
+    return pdfContentObject.str;
+}
+
+function getXAxisFromObject(pdfContentObject) {
+    return pdfContentObject.x;
+}
+function getYAxisFromObject(pdfContentObject) {
+    return pdfContentObject.y;
+}
+
+function checkPositionXandY(x,y,pdfContentObject){
+    if((getXAxisFromObject(pdfContentObject)==x) && (getYAxisFromObject(pdfContentObject)==y)){
+        return true;
+    }
+    else{
+        return false
+    }
 
 }
+
